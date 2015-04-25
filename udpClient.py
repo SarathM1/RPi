cb02b2270a549c4561e9185c81be08bd7beefcf5
Sarath M <sarathm00@gmail.com>
2015-04-25 12:52:36 +0530

    Code working perfectly
    Simplified the code

--- udpClient.py
+++ udpClient.py
@@ -3,11 +3,12 @@ import time
 obj = serial.Serial('/dev/ttyS0',9600,serial.EIGHTBITS,serial.PARITY_NONE,serial.STOPBITS_ONE,1)
 
 
-def sendAt(command,success=0,error=0,wait=1):
+def sendAt(command,success='OK',error='ERROR',wait=1):
 	"""
 	Function to send AT commands
 	to GSM Module
 	"""
+	
 	print '{0:20}'.format(command),
 	code=command + '\r\n'
 	obj.write(code)
@@ -16,7 +17,7 @@ def sendAt(command,success=0,error=0,wait=1):
 	status=checkStatus(success,error,wait)
 	return status
 
-def checkStatus(success=0,error=0,wait=1):
+def checkStatus(success='OK',error='ERROR',wait=1):
 	"""
 	Function to wait and respond for Replies from modem for each
 	AT command sent to it 
@@ -40,36 +41,35 @@ def checkStatus(success=0,error=0,wait=1):
 	
 	#print '\t((('+status+')))'
 	
-	"""string = [s.split(' ').strip() for s in status]"""
+	
 
 	string=status.split('\n')
 	string = ''.join(string)
-	string = string.replace('\r',',')
-	string = string.replace(',,','; ')
+	string = string.replace('\r',' ').replace(',,','; ')
 
-	if success==0 or error==0: # If no arguments are passed to the function,then wait for any key press
-		raw_input()
+
+	
+	if success in status:
+		#print '\t\t',
+		print '{0:20} ==> {1:50}'.format('success',string)
+		return 'success'  # success => AT Command sent
+	elif error in status:
+		print '{0:20} ==> {1:50}'.format('Error',string)
+		return 'sendError'
 	else:
-		if success in status:
-			#print '\t\t',
-			print '{0:20} ==> {1:50}'.format('success',string)
-			return 'success'  # success => AT Command sent
-		elif error in status:
-			print '{0:20} ==> {1:50}'.format('Error',string)
-			return 'sendError'
-		else:
-			print '{0:20} ==> {1:50}'.format('Other',string)
-			return 'other'
+		print '{0:20} ==> {1:50}'.format('Other',string)
+		return 'other'
 
 def sendPacket():
 	while True:
-		sendAt('ate0','OK','ERROR')
-		sendAt('at+cpin?','OK','ERROR')
-		sendAt('at+csq','OK','ERROR')
-		sendAt('at+creg?','OK','ERROR')
-		sendAt('at+cgatt?','OK','ERROR')
-		sendAt('at+cipshut','OK','ERROR')
-		status=sendAt('at+cstt="bsnlnet"','OK','ERROR')
+		print '------------------------'
+		sendAt('ate0')
+		sendAt('at+cpin?')
+		sendAt('at+csq')
+		sendAt('at+creg?')
+		sendAt('at+cgatt?')
+		sendAt('at+cipshut')
+		status=sendAt('at+cstt="bsnlnet"')
 		
 		if 'connError' in status:
 			flag='Error'
@@ -80,15 +80,15 @@ def sendPacket():
 
 		if 'success' in flag:           # Skip the rest if sendError or connError
 			sendAt('at+cifsr','.','ERROR')
-			flag = sendAt('at+cipstart="UDP","52.74.106.150","50001"','OK','ERROR')   # Warning!! Enter correct IP address and Port, 50001 -UDP,50003-TCP
+			flag = sendAt('at+cipstart="UDP","52.74.106.150","50001"')   # Warning!! Enter correct IP address and Port, 50001 -UDP,50003-TCP
 			#print 'flag = '+flag    # flag should be error if reply is CONNECT FAIL
 			sendAt('at+cipsend','>','ERROR')
 			obj.write('packet;packet;packet'+'\x1A')
-			checkStatus('OK','ERROR')
-			sendAt('at+cipclose','OK','ERROR')
+			checkStatus('SEND OK','ERROR')
+			sendAt('at+cipclose')
 			
 		else:
-			sendAt('at+cipclose','OK','ERROR')
+			sendAt('at+cipclose')
 			
 
 sendPacket()
