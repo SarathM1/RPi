f8fbb9485a24c4d93446bd3c500a4ed1c23d2cd1
Sarath M <sarathm00@gmail.com>
2015-05-15 12:47:23 +0530

    Changes to be tested

--- udpClient_python3.py
+++ udpClient_python3.py
@@ -7,6 +7,103 @@ import minimalmodbus
 import sqlite3
 import random
 
+from flask import Flask, flash
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask (__name__)
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://admin:aaggss@localhost/dreadger'
+
+class dreadger(db.Model):
+    __tablename__ = 'backfill'
+    id                  = db.Column(db.Integer, primary_key=True)
+    dreadger_name       = db.Column(db.String(25))
+    time                = db.Column(db.DateTime,unique=True)  # If not unique then there will be logical errors
+    storage_tank_level  = db.Column(db.Integer)
+    storage_tank_cap    = db.Column(db.String(25))
+    service_tank_level  = db.Column(db.Integer)
+    service_tank_cap    = db.Column(db.String(25))
+    flowmeter_1_in      = db.Column(db.Integer)
+    flowmeter_1_out     = db.Column(db.Integer)
+    engine_1_status     = db.Column(db.String(25))
+    flowmeter_2_in      = db.Column(db.Integer)
+    flowmeter_2_out     = db.Column(db.Integer)
+    engine_2_status     = db.Column(db.String(25))
+
+    def __repr__(self):
+        return self.dreadger_name+ ',' +str(self.time)+ ',' +str(self.storage_tank_level)+ ',' +\
+                self.storage_tank_cap+ ',' +str(self.service_tank_level)+ ',' +self.service_tank_cap+ ',' +\
+                str(self.flowmeter_1_in)+ ',' +str(self.flowmeter_1_out)+ ',' +self.engine_1_status+ ',' +str(self.flowmeter_2_in)+ ',' +\
+                str(self.flowmeter_2_out)+ ',' +str(self.engine_2_status)+'\n'
+
+    def __init__(self, arg):
+        
+        self.dreadger_name       = arg['dreadger_name']
+        self.time                = arg['time']
+        self.storage_tank_level  = arg['storage_tank_level']
+        self.storage_tank_cap    = arg['storage_tank_cap']
+        self.service_tank_level  = arg['service_tank_level']
+        self.service_tank_cap    = arg['service_tank_cap']
+        self.flowmeter_1_in      = arg['flowmeter_1_in']
+        self.flowmeter_1_out     = arg['flowmeter_1_out']
+        self.engine_1_status     = arg['engine_1_status']
+        self.flowmeter_2_in      = arg['flowmeter_2_in']
+        self.flowmeter_2_out     = arg['flowmeter_2_out']
+        self.engine_2_status     = arg['engine_2_status']
+    
+
+class database_backup():
+    def db_init(self):
+        try:
+            db.create_all()
+        except Exception as e:
+            print(('db_init ERROR:'+str(e)))
+            pass
+        
+    def drop_all(self):
+        db.drop_all()
+
+    def insertDb(self,arg):
+        try:
+            data=dreadger(arg)
+            db.session.add(data)
+            db.session.commit()
+        except Exception as e:
+            #flash('insertDb: '+str(e))
+            print 'insertDb: '+str(e)
+
+    def deleteDb(self,arg):
+        try:
+            dreadger.query.filter(dreadger.time == arg['time']).delete()
+            db.session.commit()
+        except Exception as e:
+            #flash('insertDb: '+str(e))
+            print 'deleteDb: '+str(e)
+    def fetchData(self):
+        try:
+            results = dreadger.query.order_by(dreadger.time.desc()).first()
+            if not results:
+                return None  # If database is empty
+            else:
+                dictRow={}
+                dictRow['dreadger_name']        = results.dreadger_name
+                dictRow['time']                 = results.time
+                dictRow['storage_tank_level']   = results.storage_tank_level
+                dictRow['storage_tank_cap']     = results.storage_tank_cap
+                dictRow['service_tank_level']   = results.service_tank_level
+                dictRow['service_tank_cap']     = results.service_tank_cap
+                dictRow['flowmeter_1_in']       = results.flowmeter_1_in
+                dictRow['flowmeter_1_out']      = results.flowmeter_1_out
+                dictRow['engine_1_status']      = results.engine_1_status
+                dictRow['flowmeter_2_in']       = results.flowmeter_2_in
+                dictRow['flowmeter_2_out']      = results.flowmeter_2_out
+                dictRow['engine_2_status']      = results.engine_2_status
+                return dictRow
+        except Exception as e:
+            #flash('insertDb: '+str(e))
+            print 'fetchData: '+str(e)
+
+
 class plc():
     def __init__(self):
         self.instrument = minimalmodbus.Instrument('/dev/ttyUSB1',1)
@@ -24,7 +121,7 @@ class plc():
 class Sim900():
     def __init__ (self):
         self.obj = serial.Serial('/dev/ttyUSB0',9600,serial.EIGHTBITS,serial.PARITY_NONE,serial.STOPBITS_ONE,1)
-        self.db=database()
+        self.db=database_backup()
     def sendAt(self,command,success='OK',error='ERROR',wait=1):
         """
         Function to send AT commands
@@ -124,7 +221,7 @@ class Sim900():
         #packet=device+';'+str(level)+';'+str(time)
 
         
-        arg['dreadger_name']        = 'dreadger_name'
+        """arg['dreadger_name']        = 'dreadger_name'
         arg['time']                 = time.localtime()
         arg['storage_tank_level']   = instrument.read_register(4096)
         arg['storage_tank_cap']     = instrument.read_register(4104)
@@ -136,6 +233,7 @@ class Sim900():
         arg['flowmeter_2_in']       = instrument.read_register(4103)
         arg['flowmeter_2_out']      = instrument.read_register(4101)
         arg['engine_2_status']      = instrument.read_register(4107)
+        """
 
         packet=''
         for key in arg:
@@ -158,71 +256,33 @@ class Sim900():
         elif case=='backfill':
             if flag=='success':    #Delete packet from database once backfill has send it to server succesfully
                 print('backfill : Success . .')
-                self.db.deleteDb('live',time,level)
+                self.db.deleteDb(arg)
             else:
                 print('device = ',device)
                 print('backfill : Failed!!')
 
         
 
-class database():
 
-    def db_init(self):
-
-        conn = sqlite3.connect("/home/wa/Documents/RPi/backup.db")
-        #conn = sqlite3.connect("/home/pi/Desktop/RPi/backup.db")
-        c=conn.cursor()
-        try:
-            c.execute("CREATE TABLE table1(device TEXT,level INT,time TEXT)")
-            conn.close()
-        except Exception as e:
-            print(('db_init ERROR:'+str(e)))
-            pass
-        finally:
-            conn.close()
-    def fetchData(self):
-        conn=sqlite3.connect("/home/wa/Documents/RPi/backup.db")
-        #conn=sqlite3.connect("/home/pi/Desktop/RPi/backup.db")
-        c=conn.cursor()
-        c.execute("SELECT * FROM table1 ORDER BY ROWID LIMIT 1")
-        data=c.fetchone()
-        conn.close()
-        return data
-    def insertDb(self,device,level,currentTime):
-
-        conn=sqlite3.connect("/home/wa/Documents/RPi/backup.db")
-        #conn=sqlite3.connect("/home/pi/Desktop/RPi/backup.db")
-        c=conn.cursor()
-        c.execute("INSERT INTO table1 values(?,?,?)",( device,str(level),str(currentTime) ))
-        conn.commit()
-        conn.close()
-    def deleteDb(self,device,time,level):
-        conn=sqlite3.connect("/home/wa/Documents/RPi/backup.db")
-        #conn=sqlite3.connect("/home/pi/Desktop/RPi/backup.db")
-        c=conn.cursor()
-        sql = "DELETE FROM table1 WHERE device=? and time=? and level=?"
-        c.execute(sql,[device,time,level])
-        conn.commit()
-        conn.close()
 
 class backFill(threading.Thread):
     
     def __init__(self,event):
         threading.Thread.__init__(self)
         self.event = event
-        self.db=database()
+        self.db=database_backup()
         self.gsm = Sim900()
 
     def run(self):
         i=1
         while True:
             self.event.wait()
-            data = self.db.fetchData()
-            if not data:
+            arg = self.db.fetchData()
+            if not arg:
                 print ('Database is empty')
                 time.sleep(1)
             else:
-                self.gsm.sendPacket('backfill',data[1],data[2],'backfill')
+                self.gsm.sendPacket(arg,'backfill')
                 time.sleep(1)
 
 class live(threading.Thread):
@@ -279,7 +339,7 @@ class live(threading.Thread):
             time.sleep(10)                   #backfill runs for 10 sec's
             
 def main():
-    db=database()
+    db=database_backup()
     db.db_init()
     event = threading.Event()
 
