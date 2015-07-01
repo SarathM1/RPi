ca11ec7ac73d143e02ad864f429ec51bced8c02f
Sarath M <sarathm00@gmail.com>
2015-07-01 16:42:26 +0530

    Changed db handler to pymysql

--- backfill.py
+++ backfill.py
@@ -1,69 +1,75 @@
-from sqlalchemy import *
+import pymysql
 from datetime import datetime as dt
 
-db = create_engine('mysql+mysqlconnector://admin:aaggss@localhost/dredger')
+conn = pymysql.connect(host="localhost", user="root", passwd="aaggss", db="dredger")
+cur = conn.cursor()
 #FUll- PAth : sqlite:////tmp/tutorial/joindemo.db
 # sudo apt-get install python3-mysql.connector
-
-
-metadata = MetaData(db)
-
-
-backfill = Table('backfill', metadata, autoload=True)
-
-
 
 
 class database_backup():
 	def insertDb(self,arg,error_code):
 		try:
-			i = backfill.insert()
-			i.execute(dredger_name = arg['dredger_name'],
-			time                = arg['time'],					
-			storage_tank_level  = arg['storage_tank_level'],
-			storage_tank_cap    = arg['storage_tank_cap'],
-			service_tank_level  = arg['service_tank_level'],
-			service_tank_cap    = arg['service_tank_cap'],
-			flowmeter_1_in      = arg['flowmeter_1_in'],
-			flowmeter_1_out     = arg['flowmeter_1_out'],
-			engine_1_status     = arg['engine_1_status'],
-			flowmeter_2_in      = arg['flowmeter_2_in'],
-			flowmeter_2_out     = arg['flowmeter_2_out'],
-			engine_2_status     = arg['engine_2_status'],
-			error_code 			= error_code,			#To get the latest error code
+			cur.execute("INSERT INTO backfill (dredger_name,\
+				time,\
+				storage_tank_level,\
+				storage_tank_cap,\
+				service_tank_level,\
+				service_tank_cap,\
+				flowmeter_1_in,\
+				flowmeter_1_out,\
+				engine_1_status,\
+				flowmeter_2_in ,\
+				flowmeter_2_out,\
+				engine_2_status,\
+				error_code)\
+			 VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);",
+			 	[arg['dredger_name'],
+			 	arg['time'],
+			 	arg['storage_tank_level'],
+			 	arg['storage_tank_cap'],
+			 	arg['service_tank_level'],
+			 	arg['service_tank_cap'],
+			 	arg['flowmeter_1_in'],
+			 	arg['flowmeter_1_out'],
+			 	arg['engine_1_status'],
+			 	arg['flowmeter_2_in'],
+			 	arg['flowmeter_2_out'],
+			 	arg['engine_2_status'],
+			 	error_code])
+			conn.commit()
 
-			)
 		except Exception as e:
 			print ('insertDb: '+str(e))
 			
 	def deleteDb(self,arg):
 		try:
-			d = backfill.delete(backfill.c.time == arg['time'])
-			d.execute()
+			cur.execute("DELETE FROM backfill where time = %s",[arg['time']])
+			conn.commit()
 		except Exception as e:
 			print ('deleteDb: '+str(e))
 	def fetchData(self):
 		try:
-			s = backfill.select().order_by(desc(backfill.c.time))
-			e = s.execute()
-			row = e.fetchone()
+			cur.execute("SELECT * FROM backfill order by time")
+			row = cur.fetchone()
+			
 			if not row:
 				return None  # If database is empty
 			else:
 				dictRow={}
-				dictRow['dredger_name']        = row.dredger_name
-				dictRow['time']                 = row.time
-				dictRow['storage_tank_level']   = row.storage_tank_level
-				dictRow['storage_tank_cap']     = row.storage_tank_cap
-				dictRow['service_tank_level']   = row.service_tank_level
-				dictRow['service_tank_cap']     = row.service_tank_cap
-				dictRow['flowmeter_1_in']       = row.flowmeter_1_in
-				dictRow['flowmeter_1_out']      = row.flowmeter_1_out
-				dictRow['engine_1_status']      = row.engine_1_status
-				dictRow['flowmeter_2_in']       = row.flowmeter_2_in
-				dictRow['flowmeter_2_out']      = row.flowmeter_2_out
-				dictRow['engine_2_status']      = row.engine_2_status
-				dictRow['error_code']			= row.error_code
+				dictRow['dredger_name']        = row[1]
+				dictRow['time']                 = row[2]
+				dictRow['storage_tank_level']   = row[3]
+				dictRow['storage_tank_cap']     = row[4]
+				dictRow['service_tank_level']   = row[5]
+				dictRow['service_tank_cap']     = row[6]
+				dictRow['flowmeter_1_in']       = row[7]
+				dictRow['flowmeter_1_out']      = row[8]
+				dictRow['engine_1_status']      = row[9]
+				dictRow['flowmeter_2_in']       = row[10]
+				dictRow['flowmeter_2_out']      = row[11]
+				dictRow['engine_2_status']      = row[12]
+				dictRow['error_code']			= row[13]
 				return dictRow
 		except Exception as e:
 			print ('fetchData: '+str(e))
