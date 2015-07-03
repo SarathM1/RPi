9090fed22423f9b5e634e2612d6c0e51a9762a88
Sarath M <sarathm00@gmail.com>
2015-07-03 13:01:26 +0530

    Better formatted output

--- debug.py
+++ debug.py
@@ -5,17 +5,17 @@ based on the bits that are set
 class errorHandler():
     def lookup(self,errType):
         return {
-        0:'boot',
-        1:'plcUsb',
-        2:'gsmUsb',
-        3:'gsmConn',
-        4:'gsmCpin',
-        5:'gsmCsq',
-        6:'gsmCreg',
-        7:'gsmCgatt',
-        8:'gsmCiicr',
-        9:'plcComm',
-        10:'serverAck',
+        0:'First boot',
+        1:'PLC 	-->	Disconnected from USB Port',
+        2:'GSM 	-->	Modem Disconnected from USB Ported',
+        3:'AT 	--> CONNECT FAIL',
+        4:'AT 	-->	Pin Code Error',
+        5:'AT 	-->	Signal Quality Error',
+        6:'AT 	-->	Sim Registration Error',
+        7:'AT 	-->	GPRS Attached error',
+        8:'AT 	-->	CIICR',
+        9:'PLC	-->	Communication Error(No Answer)',
+        10:'SERVER	-->	No ACK From SERVER',
         }.get(errType)
 
     def checkBit(self,errType,code):
@@ -27,9 +27,10 @@ class errorHandler():
         else:
             return (True if ( code &  mask) else False)
 
-obj = errorHandler()
-code = input("Enter the error code: ")
-code = int(code)
-for i in range(11):
-	if obj.checkBit(i,code):
-		print (obj.lookup(i))
+while True:
+	obj = errorHandler()
+	code = input("\nEnter the error code: ")
+	code = int(code)
+	for i in range(11):
+		if obj.checkBit(i,code):
+			print (obj.lookup(i))
