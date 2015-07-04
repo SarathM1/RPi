9c578169cfe3cb4c8c1d6aa9802d5715f48930c9
Sarath M <sarathm00@gmail.com>
2015-07-04 14:49:30 +0530

    Reduced size of columns

--- manage_db.py
+++ manage_db.py
@@ -5,21 +5,22 @@ cur = conn.cursor()
 
 def table_create():
     cur.execute('CREATE TABLE backfill (id INTEGER NOT NULL AUTO_INCREMENT,\
-     dredger_name VARCHAR(25),\
+     dredger_name VARCHAR(15),\
      time DATETIME,\
      storage_tank_level INTEGER,\
-     storage_tank_cap VARCHAR(25),\
+     storage_tank_cap VARCHAR(5),\
      service_tank_level INTEGER,\
-     service_tank_cap VARCHAR(25),\
+     service_tank_cap VARCHAR(5),\
      flowmeter_1_in INTEGER,\
      flowmeter_1_out INTEGER,\
-     engine_1_status VARCHAR(25),\
+     engine_1_status VARCHAR(5),\
      flowmeter_2_in INTEGER,\
      flowmeter_2_out INTEGER,\
-     engine_2_status VARCHAR(25),\
-     error_other VARCHAR(25),\
-     error_gsm VARCHAR(25),\
-     error_gsm_timeout VARCHAR(25),\
+     engine_2_status VARCHAR(5),\
+     error_gsm VARCHAR(20),\
+     error_main VARCHAR(20),\
+     error_timeout VARCHAR(20),\
+     error_unknown VARCHAR(20),\
      PRIMARY KEY (id),\
      UNIQUE (time))')
 
