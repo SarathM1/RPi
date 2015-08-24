29e35adf51a44d8dfc76ae6c203c12f3e81c3e11
Sarath M <sarathm00@gmail.com>
2015-08-24 16:40:22 +0530

    Indendation error

--- led.py
+++ led.py
@@ -8,77 +8,77 @@ import threading, Queue
 import random
 
 class hwThread(threading.Thread):
-    
-    def __init__(self, pin):
-        super(hwThread, self).__init__()
-        self.stoprequest = threading.Event()
+	
+	def __init__(self, pin):
+		super(hwThread, self).__init__()
+		self.stoprequest = threading.Event()
 
-        self.pin = pin
+		self.pin = pin
 
-    def run(self):
-        
-        while not self.stoprequest.isSet():
-        	print "hwThread, Run()"
-            if self.pin == pin["plc_ok"]:
-            	hw(pin["plc_ok"],plc_ok)
-            else:
-            	hw(pin["modem_ok"],modem_ok)
+	def run(self):
+		
+		while not self.stoprequest.isSet():
+			print "hwThread, Run()"
+			if self.pin == pin["plc_ok"]:
+				hw(pin["plc_ok"],plc_ok)
+			else:
+				hw(pin["modem_ok"],modem_ok)
 
-    def join(self, timeout=None):
-        self.stoprequest.set()
-        super(hwThread, self).join(timeout)
+	def join(self, timeout=None):
+		self.stoprequest.set()
+		super(hwThread, self).join(timeout)
 
 
 class debugThread(threading.Thread):
-    
-    def __init__(self, pin, q):
-        super(debugThread, self).__init__()
-        self.q = q
-        self.pin = pin
-        self.stoprequest = threading.Event()
+	
+	def __init__(self, pin, q):
+		super(debugThread, self).__init__()
+		self.q = q
+		self.pin = pin
+		self.stoprequest = threading.Event()
 
-    def run(self):
-        
-        while not self.stoprequest.isSet():
-        	print "\n\twaiting for Queue, PIN: "+str(self.pin)+"\n"
-        	status = self.q.get()
-        	print "Done waiting: "+str(status)
-        	
-        	if status == "off":
-        		off(self.pin)
+	def run(self):
+		
+		while not self.stoprequest.isSet():
+			print "\n\twaiting for Queue, PIN: "+str(self.pin)+"\n"
+			status = self.q.get()
+			print "Done waiting: "+str(status)
+			
+			if status == "off":
+				off(self.pin)
 
-    		elif status == "on":
-    			blink_led(self.pin,0.5)
+			elif status == "on":
+				blink_led(self.pin,0.5)
 
-    def join(self, timeout=None):
-        self.stoprequest.set()
-        super(debugThread, self).join(timeout)
+	def join(self, timeout=None):
+		self.stoprequest.set()
+		super(debugThread, self).join(timeout)
 
 class commStatusThread(threading.Thread):
-    
-    def __init__(self, pin, q):
-        super(commStatusThread, self).__init__()
-        self.q = q
-        self.pin = pin
-        self.stoprequest = threading.Event()
+	
+	def __init__(self, pin, q):
+		super(commStatusThread, self).__init__()
+		self.q = q
+		self.pin = pin
+		self.stoprequest = threading.Event()
 
-    def run(self):
-        
-        while not self.stoprequest.isSet():
-        	status = self.q.get()
-        	
-        	if status == "off":
-        		off(self.pin)
+	def run(self):
+		
+		while not self.stoprequest.isSet():
+			status = self.q.get()
+			
+			if status == "off":
+				off(self.pin)
 
-    		elif status == "live_send_ok":
-    			blink_led(self.pin,1)
+			elif status == "live_send_ok":
+				blink_led(self.pin,1)
 
-    		elif status == "backfill_send_ok":
-    			blink_led(self.pin,0.1)
+			elif status == "backfill_send_ok":
+				blink_led(self.pin,0.1)
 
-    def join(self, timeout=None):
-        self.stoprequest.set()
-        super(commStatusThread, self).join(timeout)
+	def join(self, timeout=None):
+		self.stoprequest.set()
+		super(commStatusThread, self).join(timeout)
 
 def hw(pin,status):
 	"""
