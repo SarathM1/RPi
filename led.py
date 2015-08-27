c275409d3bf56ac636f30025b30490441b90544e
Sarath M <sarathm00@gmail.com>
2015-08-27 12:01:33 +0530

    Final code, Status led's working, except GSM, at timeout

--- led.py
+++ led.py
@@ -18,7 +18,7 @@ class plc_ok_th(threading.Thread):
 		while True:
 			
 			try:
-				status = self.q.get(True,0.5)		# To make queue non-blocking
+				status = self.q.get(True,2)		# To make queue non-blocking
 			except Empty as e:
 				pass
 			with self.q.mutex:							# TRY UNCOMMENTING, IF QUEUE SIZE INCREASES
