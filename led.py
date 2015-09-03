7cfc6311e6a5b93dc07d0d5ae0bc891341b02ed6
Sarath M <sarathm00@gmail.com>
2015-09-03 16:23:19 +0530

    Debugging. .

--- led.py
+++ led.py
@@ -37,6 +37,7 @@ class plc_ok_th(threading.Thread):
 
 	def join(self,timeout = None):
 		self.stopEvent.set()
+		gpio.cleanup()
 		threading.Thread.join(self,timeout)
 
 class modem_ok_th(threading.Thread):
@@ -65,6 +66,7 @@ class modem_ok_th(threading.Thread):
 
 	def join(self,timeout = None):
 		self.stopEvent.set()
+		gpio.cleanup()
 		threading.Thread.join(self,timeout)
 
 def plc_check(pin_no,status):
